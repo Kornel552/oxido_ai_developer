26bfb7b5e3b5a5cfe4540dc4197f96cd7437ed84
Kornel552 <lenrok996@gmail.com>
2024-11-15 08:29:17 +0100

    first test

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 from dotenv import load_dotenv
 import openai
 import os
+
+load_dotenv()
+openai.api_key = os.getenv("API_KEY")
+model ='gpt-3.5-turbo'
+
+response = openai.ChatCompletion.create(
+    model=model,
+    messages=[
+        {
+            'role': 'system',
+            'content': 'Return full sentence'
+        },
+        {
+            'role': 'user',
+            'content': 'say hello'
+        }
+    ],
+    temperature=1,
+    max_tokens=256,
+)
+
+resp = response.choices[0].message.content
+print(resp)
